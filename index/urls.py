a4b9cb10ec6a97d81608d6b228265f9a474bb3f7
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-04-09 20:10:57 -0300

    Agregado de template bike_shop.html

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, nosotros, about_us
+from .views import index, nosotros, about_us, bike_shop
 
 urlpatterns = [
     path('', index, name='index'),
     path('nosotros/', nosotros, name='nosotros'),
-    path('about_us/', about_us, name='about_us')
+    path('about_us/', about_us, name='about_us'),
+    path('bike_shop/', bike_shop, name='bike_shop')
 ]
--- index/views.py
+++ index/views.py
@@ -8,3 +8,6 @@ def nosotros(request):
 
 def about_us(request):
     return render(request, 'index/about_us.html')
+
+def bike_shop(request):
+    return render(request, 'index/bike_shop.html')
